984e09c8f4287e5f2fb8016854c0f674483c80cd
Shoshana Anna Reich <sreich@pfe27.nas.nasa.gov>
2024-05-03 08:53:22 -0700

    fix path to script

--- submit_swot_regridding.sh
+++ submit_swot_regridding.sh
@@ -5,11 +5,14 @@
 #PBS -o outfile
 #PBS -e errfile
 
-source activate py38
 
 limit stacksize unlimited
 
 date +%Y/%m/%d_%H:%M:%S.%3N >> regrid_swot_timing.txt
+
+module use -a /swbuild/analytix/tools/modulefiles
+module load miniconda3/v4
+source activate py38
 
 #cd /home3/mgoldbe1/smart_cables/synthetic_data_extraction/
 parallel --slf $PBS_NODEFILE -j 3 -a /home3/sreich/obsfit_preproc/test_runs
