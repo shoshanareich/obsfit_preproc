3d1e99045d93ac9689fbc683cabf5e8da2b602f1
Shoshana Anna Reich <sreich@pfe22.nas.nasa.gov>
2024-05-13 10:24:04 -0700

    fixed typos

--- submit_swot_regridding.sh
+++ submit_swot_regridding.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N regrid_swot
-#PBS -l select=42:ncpus=28:model=bro
-#PBS -l walltime=00:05:00
+#PBS -l select=21:ncpus=28:model=bro
+#PBS -l walltime=00:15:00
 #PBS -o outfile
 #PBS -e errfile
 
@@ -15,6 +15,6 @@ module load miniconda3/v4
 source activate py38
 
 #cd /home3/mgoldbe1/smart_cables/synthetic_data_extraction/
-parallel --slf $PBS_NODEFILE -j 1156 -a /home3/sreich/obsfit_preproc/cycle9_runs
+parallel --slf $PBS_NODEFILE -j 28 -a /home3/sreich/obsfit_preproc/cycle9_runs
 
 date +%Y/%m/%d_%H:%M:%S.%3N >> regrid_swot_timing.txt
